92581f5ccad3289f8e2b58a2f6b237eb476ec05b
Christian Berndt <christian.berndt@liferay.com>
2025-03-17 11:15:09 +0100

    feat: add scripts for investigating scheduler status.

---
+++ src/main/groovy/InvestigateScheduledJobs.groovy
@@ -0,0 +1,13 @@
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelperUtil
+import com.liferay.portal.kernel.scheduler.StorageType
+import com.liferay.portal.kernel.scheduler.messaging.SchedulerResponse
+
+schedulerResponses = SchedulerEngineHelperUtil.getScheduledJobs(StorageType.PERSISTED)
+
+for (SchedulerResponse schedulerResponse : schedulerResponses ) {
+    out.println(SchedulerEngineHelperUtil.getStartTime(schedulerResponse))
+    out.println(SchedulerEngineHelperUtil.getEndTime(schedulerResponse))
+    out.println(SchedulerEngineHelperUtil.getNextFireTime(schedulerResponse))
+    out.println(SchedulerEngineHelperUtil.getJobState(schedulerResponse))
+    out.println(schedulerResponse)
+}
