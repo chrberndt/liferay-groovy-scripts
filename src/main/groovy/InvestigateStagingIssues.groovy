6d0c31ebe781a40bfaf14e11e23d8f740d328945
Christian Berndt <christian.berndt@liferay.com>
2025-06-26 12:59:11 +0200

    Update InvestigateStagingIssues.groovy

--- src/main/groovy/InvestigateStagingIssues.groovy
+++ src/main/groovy/InvestigateStagingIssues.groovy
@@ -1,5 +1,6 @@
 import com.liferay.portal.kernel.dao.orm.OrderFactoryUtil
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil
+import com.liferay.portal.kernel.model.Layout
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil
 
 friendlyURL = "/bmw-product"
@@ -13,3 +14,14 @@ dynamicQuery.addOrder(OrderFactoryUtil.asc("groupId"))
 layouts = LayoutLocalServiceUtil.dynamicQuery(dynamicQuery, 0, 100)
 
 out.println("layouts.size(): " + layouts.size())
+
+for (Layout layout : layouts) {
+
+    groupId = layout.groupId
+    plid = layout.plid
+
+    out.println("groupId: " + groupId)
+    out.println("plid: " + plid)
+
+
+}
