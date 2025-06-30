54df5561910e587593604d960bc7d11a9abc11db
Christian Berndt <christian.berndt@liferay.com>
2025-06-30 09:45:23 +0200

    Update InvestigateStagingIssues.groovy

--- src/main/groovy/InvestigateStagingIssues.groovy
+++ src/main/groovy/InvestigateStagingIssues.groovy
@@ -16,39 +16,31 @@ import com.liferay.portal.kernel.service.ServiceContext
 import com.liferay.portal.kernel.util.LocaleUtil
 import com.liferay.portal.kernel.workflow.WorkflowConstants
 
-friendlyURL = "/bmw-product"
-
-// 1. determine ids of layouts
-
-dynamicQuery = LayoutLocalServiceUtil.dynamicQuery()
-dynamicQuery.add(RestrictionsFactoryUtil.eq("friendlyURL", friendlyURL))
-dynamicQuery.addOrder(OrderFactoryUtil.asc("groupId"))
 
 stagingGroupId = 1386890L
+//stagingGroupId = 33700L
 liveGroupId = 419619L
+//liveGroupId = 34559L
+
+dynamicQuery = LayoutLocalServiceUtil.dynamicQuery()
+dynamicQuery.add(RestrictionsFactoryUtil.eq("groupId", liveGroupId))
+//dynamicQuery.add(RestrictionsFactoryUtil.eq("groupId", stagingGroupId))
+dynamicQuery.addOrder(OrderFactoryUtil.asc("plid"))
 
 anonymousUserId = 0
 
+layouts = LayoutLocalServiceUtil.dynamicQuery(dynamicQuery, 0, 5000)
 
-layouts = LayoutLocalServiceUtil.dynamicQuery(dynamicQuery, 0, 100)
-
-// out.println("layouts.size(): " + layouts.size())
+out.println("layouts.size(): " + layouts.size())
 
 for (Layout layout : layouts) {
 
     groupId = layout.groupId
     plid = layout.plid
 
-    out.println("groupId: " + groupId)
-    out.println("plid: " + plid)
-
     portletPreferences = PortletPreferencesLocalServiceUtil.getPortletPreferencesByPlid(layout.plid)
 
-    out.println("portletPreferences.size(): " + portletPreferences.size())
-    out.println()
-
     for (PortletPreferences preferences : portletPreferences) {
-        // out.println(preferences.portletPreferencesId)
 
         prefs = PortletPreferenceValueLocalServiceUtil.getPreferences(preferences)
 
@@ -58,105 +50,89 @@ for (Layout layout : layouts) {
 
             journalArticle = JournalArticleLocalServiceUtil.fetchLatestArticle(layout.groupId, articleId, WorkflowConstants.STATUS_APPROVED)
 
-            content = journalArticle.getContent()
+            if (journalArticle != null) {
 
-            // out.println(content)
+                content = journalArticle.getContent()
 
-            ddmStructureId = journalArticle.getDDMStructureId()
+                ddmStructureId = journalArticle.getDDMStructureId()
 
-            ddmStructure = DDMStructureLocalServiceUtil.fetchDDMStructure(ddmStructureId)
+                ddmStructure = DDMStructureLocalServiceUtil.fetchDDMStructure(ddmStructureId)
 
-            // out.println("ddmStructure: " + ddmStructure)
+                if (ddmStructure != null) {
 
-            if (ddmStructure != null) {
-                // out.println(ddmStructure)
+                    ddmForm = ddmStructure.getDDMForm()
 
-                ddmForm = ddmStructure.getDDMForm()
+                    id = journalArticle.id
 
-                id = journalArticle.id
+                    ddmFormValues = DDMFieldLocalServiceUtil.getDDMFormValues(ddmForm, id)
 
-                ddmFormValues = DDMFieldLocalServiceUtil.getDDMFormValues(ddmForm, id)
+                    ddmFormFieldValues = ddmFormValues.getDDMFormFieldValues()
 
-                // out.println("ddmFormValues: " + ddmFormValues)
+                    for (DDMFormFieldValue ddmFormFieldValue : ddmFormFieldValues) {
 
-                ddmFormFieldValues = ddmFormValues.getDDMFormFieldValues()
+                        firstNestedFormFieldValues = ddmFormFieldValue.getNestedDDMFormFieldValues()
 
-                // out.println("ddmFormFieldValues: " + ddmFormFieldValues)
+                        for (DDMFormFieldValue firstNestedFormFieldValue : firstNestedFormFieldValues) {
 
-                for (DDMFormFieldValue ddmFormFieldValue : ddmFormFieldValues) {
+                            secondNestedFormFieldValues = firstNestedFormFieldValue.getNestedDDMFormFieldValues()
 
-                    firstNestedFormFieldValues = ddmFormFieldValue.getNestedDDMFormFieldValues()
+                            for (DDMFormFieldValue secondNestedFormFieldValue : secondNestedFormFieldValues) {
 
-                    out.println("firstNestedFormFieldValues.size(): " + firstNestedFormFieldValues.size())
+                                value = secondNestedFormFieldValue.value
 
-                    for (DDMFormFieldValue firstNestedFormFieldValue : firstNestedFormFieldValues) {
+                                if (value != null) {
 
-                        secondNestedFormFieldValues = firstNestedFormFieldValue.getNestedDDMFormFieldValues()
+                                    localizedValue = value.getString(LocaleUtil.siteDefault)
 
-                        out.println("secondNestedFormFieldValues.size(): " + secondNestedFormFieldValues.size())
+                                    if (localizedValue != null && localizedValue.startsWith("{")) {
 
-                        for (DDMFormFieldValue secondNestedFormFieldValue : secondNestedFormFieldValues) {
+                                        out.println()
 
-                            value = secondNestedFormFieldValue.value
+                                        jsonObject = JSONFactoryUtil.createJSONObject(localizedValue)
+                                        out.println("jsonObject: " + jsonObject)
 
-                            // out.println("value: " + value)
+                                        imageGroupId = jsonObject.getLong("groupId");
+                                        out.println("imageGroupId: " + imageGroupId)
 
-                            if (value != null) {
+                                        fileEntryId = jsonObject.getLong("fileEntryId")
+                                        out.println("fileEntryId: " + fileEntryId)
 
-                                localizedValue = value.getString(LocaleUtil.siteDefault)
+                                        uuid = jsonObject.getString("uuid")
+                                        out.println("uuid: " + uuid)
 
-                                // out.println("localizedValue: " + localizedValue)
+                                        if (fileEntryId > 0) {
 
-                                if (localizedValue != null && localizedValue.startsWith("{")) {
+                                            stagingFileEntry = DLAppLocalServiceUtil.fetchFileEntryByExternalReferenceCode(stagingGroupId, uuid)
+                                            liveFileEntry = DLAppLocalServiceUtil.fetchFileEntryByExternalReferenceCode(liveGroupId, uuid)
 
-                                    jsonObject = JSONFactoryUtil.createJSONObject(localizedValue)
+                                            out.println("stagingFileEntry: " + stagingFileEntry)
+                                            out.println("liveFileEntry: " + liveFileEntry)
 
-                                    imageGroupId = jsonObject.getLong("groupId");
-                                    out.println("imageGroupId: " + imageGroupId)
+                                            if ((stagingFileEntry != null) && (liveFileEntry != null)) {
 
-                                    fileEntryId = jsonObject.getLong("fileEntryId")
-                                    out.println("fileEntryId: " + fileEntryId)
+                                                groupIdFrom = "{\"groupId\":\"" + stagingFileEntry.groupId + "\""
+                                                groupIdTo = "{\"groupId\":\"" + liveFileEntry.groupId + "\""
 
-                                    uuid = jsonObject.getString("uuid")
-                                    out.println("uuid: " + uuid)
-                                    out.println()
+                                                fileEntryIdFrom = "\"fileEntryId\":\"" + stagingFileEntry.fileEntryId + "\""
+                                                fileEntryIdTo = "\"fileEntryId\":\"" + liveFileEntry.fileEntryId + "\""
 
-                                    if (fileEntryId > 0) {
+                                                content = content.replace(groupIdFrom, groupIdTo)
+                                                content = content.replace(fileEntryIdFrom, fileEntryIdTo)
 
-                                        stagingFileEntry = DLAppLocalServiceUtil.getFileEntryByUuidAndGroupId(uuid, stagingGroupId)
-                                        liveFileEntry = DLAppLocalServiceUtil.getFileEntryByUuidAndGroupId(uuid, liveGroupId)
+                                                serviceContext = new ServiceContext()
+                                                serviceContext.setScopeGroupId(journalArticle.groupId)
 
-//                                        out.println("stagingFileEntry: " + stagingFileEntry)
-//                                        out.println("liveFileEntry: " + liveFileEntry)
+                                                ExportImportThreadLocal.setLayoutImportInProcess(true)
 
-                                        groupIdFrom = "{\"groupId\":\"" + stagingFileEntry.groupId + "\""
-                                        groupIdTo = "{\"groupId\":\"" + liveFileEntry.groupId + "\""
-
-                                        fileEntryIdFrom = "\"fileEntryId\":\"" + stagingFileEntry.fileEntryId + "\""
-                                        fileEntryIdTo = "\"fileEntryId\":\"" + liveFileEntry.fileEntryId + "\""
-
-//                                        out.println(content)
-
-                                        content = content.replace(groupIdFrom, groupIdTo)
-                                        content = content.replace(fileEntryIdFrom, fileEntryIdTo)
-
-//                                        out.println(content)
-
-                                        out.println("articleId: " + journalArticle.articleId)
-                                        out.println("version: " + journalArticle.version)
-
-                                        serviceContext = new ServiceContext()
-                                        serviceContext.setScopeGroupId(journalArticle.groupId)
-
-                                        ExportImportThreadLocal.setLayoutImportInProcess(true)
-
-                                        JournalArticleLocalServiceUtil.updateArticle(anonymousUserId,
-                                                journalArticle.groupId,
-                                                journalArticle.folderId,
-                                                journalArticle.articleId,
-                                                journalArticle.version,
-                                                content,
-                                                serviceContext)
+//                                                JournalArticleLocalServiceUtil.updateArticle(anonymousUserId,
+//                                                        journalArticle.groupId,
+//                                                        journalArticle.folderId,
+//                                                        journalArticle.articleId,
+//                                                        journalArticle.version,
+//                                                        content,
+//                                                        serviceContext)
+                                            }
+                                        }
                                     }
                                 }
                             }
